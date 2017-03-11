621aeb1b55f1ab81ab6cb20d070a9cd8c9fe7dba
Hans Kurniadi <hans.d.k@live.com>
2017-03-11 00:12:38 +0000

    add total duration function

--- music-library.js
+++ music-library.js
@@ -24,20 +24,35 @@ Playlist.prototype.overallRating = function() {
   }, 0);
   let avgRating = sumRatings/ratings.length;
   // console.log('overall rating', avgRating);
+  return avgRating;
+}
+
+Playlist.prototype.totalDuration = function() {
+  let durations = this.tracks.map((track) => {
+    return track.length;
+  });
+  let totalDuration = durations.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+  // console.log('total duration is', totalDuration);
+  return totalDuration;
 }
 
 let musicLibrary = new Library('firstLib', 'cool guy');
-console.log('Library: ', musicLibrary);
+// console.log('Library: ', musicLibrary);
 
 let myPlaylist1 = new Playlist('pl1');
 musicLibrary.playlists.push(myPlaylist1);
-console.log("my first playlist", myPlaylist1);
 
-console.log('Library after: ', musicLibrary);
+
+// console.log('Library after: ', musicLibrary);
 
 let track1 = new Track("t1", 5, 300);
 let track2 = new Track("t2", 2, 240);
 myPlaylist1.tracks.push(track1);
 myPlaylist1.tracks.push(track2);
+console.log("my first playlist", myPlaylist1);
+
 
 myPlaylist1.overallRating();
+myPlaylist1.totalDuration();
