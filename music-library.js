9fed8542bbe9a7970a01a7cebe164f76b9c5c607
Hans Kurniadi <hans.d.k@live.com>
2017-03-11 00:05:03 +0000

    initial commit

--- music-library.js
+++ music-library.js
@@ -1,4 +1,43 @@
 function Library(name, creator) {
   this.name = name;
   this.creator = creator;
+  this.playlists =[];
 }
+
+function Playlist(name) {
+  this.name = name;
+  this.tracks = [];
+};
+
+function Track(title, rating, length) {
+    this.title = title;
+    this.rating = rating;
+    this.length = length;
+}
+
+Playlist.prototype.overallRating = function() {
+  let ratings = this.tracks.map((track) => {
+    return track.rating;
+  });
+  let sumRatings = ratings.reduce((prev, curr) => {
+    return prev + curr;
+  }, 0);
+  let avgRating = sumRatings/ratings.length;
+  // console.log('overall rating', avgRating);
+}
+
+let musicLibrary = new Library('firstLib', 'cool guy');
+console.log('Library: ', musicLibrary);
+
+let myPlaylist1 = new Playlist('pl1');
+musicLibrary.playlists.push(myPlaylist1);
+console.log("my first playlist", myPlaylist1);
+
+console.log('Library after: ', musicLibrary);
+
+let track1 = new Track("t1", 5, 300);
+let track2 = new Track("t2", 2, 240);
+myPlaylist1.tracks.push(track1);
+myPlaylist1.tracks.push(track2);
+
+myPlaylist1.overallRating();
